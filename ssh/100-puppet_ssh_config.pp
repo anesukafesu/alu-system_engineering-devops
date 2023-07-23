77b5798a00523a5269fd6384a79b59a21c063114
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-07-23 20:28:13 +0000

    100

---
+++ ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,7 @@
+class { 'ssh::client':
+  ssh_config          => {
+    'Host'                  => '34.234.88.145',
+    'IdentityFile'          => '~/.ssh/school',
+    'PasswordAuthentication' => 'no',
+  },
+}
