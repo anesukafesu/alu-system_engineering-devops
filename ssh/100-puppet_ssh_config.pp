b82e6e8348c1692ab40f768bd58d3fa892683ddb
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2023-07-23 21:13:16 +0000

    100

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,6 @@
 # Puppet script to auto-configure some connection options to my server
 include ssh::client
 class { 'ssh::client':
-  user_ssh_config     => '~/.ssh/config,
   ssh_config          => {
     'Host'                  => '34.234.88.145',
     'IdentityFile'          => '~/.ssh/school',
